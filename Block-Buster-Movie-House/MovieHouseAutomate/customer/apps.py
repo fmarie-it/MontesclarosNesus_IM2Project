3489644a341bfa2b483c361fd9e2fc0455490d21
fmarie-it <f.marieit@gmail.com>
2021-12-02 10:15:19 +0800

    Migration

--- Block-Buster-Movie-House/MovieHouseAutomate/customer/apps.py
+++ Block-Buster-Movie-House/MovieHouseAutomate/customer/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class CustomersConfig(AppConfig):
-    name = 'customers'
+class CustomerConfig(AppConfig):
+    name = 'customer'
--- Block-Buster-Movie-House/MovieHouseAutomate/dvd/admin.py
+++ Block-Buster-Movie-House/MovieHouseAutomate/dvd/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import DVD
+from .models import *
 
 # Register your models here.
-
-#admin.site.Register(dvd)
+admin.site.register(DVD)
